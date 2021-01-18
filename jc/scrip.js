96c1af1bac5a8f9c558f89d44935db5f87355187
darkbazory <sonia.l.ortega@gmail.com>
2021-01-18 09:05:14 +0100

    jscrip sonia

--- jc/scrip.js
+++ jc/scrip.js
@@ -1,4 +1,10 @@
-function sonia() {
-    var soniaBoton = document.getElementById("menu");
-    soniaBoton.style.width = "300px";
-}
+
+function BarraMenu() {
+    var Barbutton = document.getElementById("menu");
+    Barbutton.style.width = "300px"
+    if (Barbutton.style.display === "block") {
+        Barbutton.style.display = "none";
+    } else {
+        Barbutton.style.display = "block";
+    }
+  }
