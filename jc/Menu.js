b5ab56799636d2f03325f91a83d9652bd95cc8f9
darkbazory <sonia.l.ortega@gmail.com>
2021-02-03 09:52:14 +0100

    index movil sonia

--- jc/Menu.js
+++
@@ -1,9 +0,0 @@
-  
-console.log("Ejecutando")
-function cambiarClase(){
-    let siteNav = document.getElementById('site-nav');
-        siteNav.classList.toggle('site-nav-open');
-    let menuOpen = document.getElementById('menu-toggle');
-        menuOpen.classList.toggle('menu-open');    
-        
-}
