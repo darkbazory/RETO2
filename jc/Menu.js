6b9b96efefc3d969bb685912a2aad7b20b016319
darkbazory <sonia.l.ortega@gmail.com>
2021-02-23 20:29:54 +0100

    comentarios codigo

--- jc/Menu.js
+++ jc/Menu.js
@@ -1,8 +1,9 @@
 console.log("Ejecutando")
-function cambiarClase(){
-    let siteNav = document.getElementById('site-nav');
-        siteNav.classList.toggle('site-nav-open');
-    let menuOpen = document.getElementById('menu-toggle');
+function cambiarClase(){ /*llamamos a la funcion cambiarclase*/
+    let siteNav = document.getElementById('site-nav'); /* Declaramos la variable siteNave*/ 
+        siteNav.classList.toggle('site-nav-open'); /**Aqui añadimos la clase que queramos que añada o quite */
+    let menuOpen = document.getElementById('menu-toggle');/** unimos el menu open con toggle para que al hacer
+    click haga la transformación que pusimos en el css */
         menuOpen.classList.toggle('menu-open');
 
 }
